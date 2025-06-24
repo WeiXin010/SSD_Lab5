3bfef765c28dae044ccdfb358de52aa98b375082
WeiXin010 <145105915+WeiXin010@users.noreply.github.com>
2025-06-24 09:42:21 +0800

    modify app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-} from 'react-router-dom';
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
+//     Link,
+// } from 'react-router-dom';
 
-function LoginPage() {
-    return (
-        <div className="Login">
-            <h2>Login</h2>
-            <form id="loginForm">
-                <input type="text" id="username" placeholder="Username" required />
-                <input type="password" id="password" placeholder="Password" required />
-                <button type="submit">Login</button>
-                <div id="errorMsg" class="error"></div>
-            </form>
-        </div>
+// function LoginPage() {
+//     return (
+//         <div className="Login">
+//             <h2>Login</h2>
+//             <form id="loginForm">
+//                 <input type="text" id="username" placeholder="Username" required />
+//                 <input type="password" id="password" placeholder="Password" required />
+//                 <button type="submit">Login</button>
+//                 <div id="errorMsg" class="error"></div>
+//             </form>
+//         </div>
 
-    );
-}
+//     );
+// }
 
-function Home() {
+// function Home() {
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <p>
+//                     Tell me why! Aint nothing but a heart break.
+//                 </p>
+//                 <p>
+//                     Ruby-chan! HAIIIII! Nani gatsuki?
+//                 </p>
+// 	    	<p>
+// 		    Choco Minto youri mo.....
+// 	    	</p>
+//                 <a
+//                     className="App-link"
+//                     href="https://reactjs.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     Learn React
+//                 </a>
+//             </header>
+//         </div>
+//     );
+// }
+
+// function App() {
+//     return (
+//         <Router>
+//             <nav>
+//                 <Link to="/">Home</Link> | {" "}
+//                 <Link to="/login">About</Link>
+//             </nav>
+
+//             <Routes>
+//                 <Route path="/" element={<Home />} />
+//                 <Route path="/login" element={<LoginPage />} />
+//             </Routes>
+
+//         </Router>
+
+//     );
+// }
+
+function App() {
     return (
         <div className="App">
             <header className="App-header">
@@ -34,9 +79,9 @@ function Home() {
                 <p>
                     Ruby-chan! HAIIIII! Nani gatsuki?
                 </p>
-	    	<p>
-		    Choco Minto youri mo.....
-	    	</p>
+                <p>
+                    Choco Minto youri mo.....
+                </p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
@@ -47,24 +92,6 @@ function Home() {
                 </a>
             </header>
         </div>
-    );
-}
-
-function App() {
-    return (
-        <Router>
-            <nav>
-                <Link to="/">Home</Link> | {" "}
-                <Link to="/login">About</Link>
-            </nav>
-
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<LoginPage />} />
-            </Routes>
-
-        </Router>
-
     );
 }
 
