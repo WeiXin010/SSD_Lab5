01940d14cbdc45e4470aab57d258d9f8b4b6109e
WeiXin010 <145105915+WeiXin010@users.noreply.github.com>
2025-06-29 11:40:21 +0800

    nav bar with file upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Weather from "./pages/Weather";
+import FileUpload from "./pages/FileUpload";
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
@@ -13,12 +14,14 @@ function App() {
             <nav>
                 <Link to="/">Home</Link> | {" "}
                 <Link to="/login">Login</Link> | {" "}
-                <Link to="/weather">Weather Forecast</Link>
+                <Link to="/weather">Weather Forecast</Link> | {" "}
+                <Link to="/fileUpload">File Upload</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/weather" element={<Weather />} />
+                <Route path="/fileUplaod" element={<FileUpload />} />
             </Routes>
         </Router>
     );
